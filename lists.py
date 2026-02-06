44eb795f673e5c972c85dbd6a212663f42805dab
Shivam Anand <108640569+Shivamanand221@users.noreply.github.com>
2026-02-06 17:26:41 +0530

    added lists file

---
+++ lists.py
@@ -0,0 +1,12 @@
+marks = [3,4,5, "Hello", True]
+print(marks)
+print(marks[0])
+print(marks[-1])
+print(len(marks))
+print(marks[:])
+print(marks[2:5])
+print(marks[1:-1])
+print(marks[5:1])
+print(marks[0:5:2])
+lst=[i for i in range(10,20,2)]
+print(lst)
