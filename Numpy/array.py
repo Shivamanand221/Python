459b319179457444f6c8f118d8b5c72e9adca648
Shivam Anand <108640569+Shivamanand221@users.noreply.github.com>
2026-03-12 03:49:51 +0530

    Numpy Work

---
+++ Numpy/array.py
@@ -0,0 +1,13 @@
+import numpy as np
+a= np.array([1,2,3])
+print(a*2)
+print(np.zeros((2,3)))
+print(np.ones((2,3))  )
+print(np.eye(3))
+print(np.random.rand(2,3))
+print(np.arange(0,10,2))
+b=np.array([[1,2,3],
+          [4,5,6]])
+print(b.shape)
+print(b.ndim)
+print(b.size)
