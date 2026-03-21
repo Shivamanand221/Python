adf3b82ec84c3b9dbf523749e50bf53db102d325
Shivam Anand <108640569+Shivamanand221@users.noreply.github.com>
2026-03-21 23:39:12 +0530

    Modified vectorization file

--- Numpy/Vectorization.py
+++ Numpy/Vectorization.py
@@ -1,4 +1,13 @@
 import numpy as np
 
-v1=np.array([[1, 2, 3]])
+v1=np.array([[1, 2, 3, 4, 5]])
 print(v1*3+1)
+print(v1**2+1)
+
+a = np.array([1,2,3])
+b = np.array([10,20,30])
+print(a+b)
+
+#Normalize
+n1= (v1-np.min(v1))/(np.max(v1)-np.min(v1))
+print(n1)
