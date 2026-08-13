44f0b28ee95f3d994798de154db7bf605d9d119d
Shivam Anand <108640569+Shivamanand221@users.noreply.github.com>
2026-08-14 00:38:35 +0530

    Added headers file in fastapi

--- FastAPI/headers.py
+++ FastAPI/headers.py
@@ -7,3 +7,29 @@ def get_info(user_agent: str= Header()):
     return {
         "user_agent": user_agent
     }
+
+app1= FastAPI()
+
+@app1.get("/info")
+def get_info(
+    user_agent: str = Header(),
+    accept: str = Header()
+):
+    return {
+        "user_agent": user_agent,
+        "accept": accept
+    }
+
+app2 = FastAPI()
+
+@app2.get("/profile")
+def get_profile(
+    user_agent: str = Header(),
+    accept: str = Header(),
+    authorization: str = Header()
+):
+    return {
+        "user_agent": user_agent,
+        "accept": accept,
+        "authorization": authorization
+    }
